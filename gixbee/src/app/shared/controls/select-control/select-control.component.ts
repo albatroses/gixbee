3151d113982e5ae5333c280746682710d2efa8b5
gkv0013 <66899128+gkv0013@users.noreply.github.com>
2024-12-08 01:45:18 +0530

    d

---
+++ gixbee/src/app/shared/controls/select-control/select-control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-control',
+  imports: [],
+  templateUrl: './select-control.component.html',
+  styleUrl: './select-control.component.scss'
+})
+export class SelectControlComponent {
+
+}
